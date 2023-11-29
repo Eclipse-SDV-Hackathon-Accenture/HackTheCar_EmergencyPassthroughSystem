406e8b0ded84457a257db6810f02ef1b15acb61b
afri-bit <20866800+afri-bit@users.noreply.github.com>
2023-11-29 14:06:39 +0100

    Added some structure

--- src/mqtt-bridge/mqtt_bridge.py
+++ src/mqtt-bridge/mqtt_bridge.py
@@ -1,3 +1,8 @@
+"""
+Application to bridge communication between eCAL and MQTT
+
+"""
+
 import sys
 
 import ecal.core.core as ecal_core
@@ -7,28 +12,25 @@ from ecal.core.subscriber import StringSubscriber
 
 client = mqtt.Client("", clean_session=True, userdata=None, protocol=mqtt.MQTTv31)
 
-
-# The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
+    """
+    Callback method 
+    """
+
     print("Connected with result code " + str(rc))
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
     client.subscribe("#")
 
 
-# The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
 
-    # Create a String Publisher that publishes on the topic "hello_world_python_topic"
     pub = StringPublisher("MQTT_Message")
 
     pub.send(str(msg.payload))
 
 
 def callback(topic_name, msg, time):
-    # print("Received: {}".format(msg))
     print(msg)
     client.publish(topic_name, msg)
     client.publish
@@ -41,7 +43,7 @@ if __name__ == "__main__":
 
     client.on_connect = on_connect
     client.on_message = on_message
-    client.username_pw_set("sdv_ecal", "SDV_ecal123")
+    client.username_pw_set("", "")
     client.tls_set()
     client.connect("5e57e5cfb02f468ba5e49adade286f4b.s1.eu.hivemq.cloud", 8883, 60)
 
